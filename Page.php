639ec2d6f40514d250229ac1fc68ea61659fb5b3
Daumantas Urbanavicius <daumantas@aruodas.lt>
2018-11-20 19:08:55 +0200

    update class

---
+++ Page.php
@@ -0,0 +1,96 @@
+<?php
+
+require_once 'ActiveRecord.php';
+
+class Page extends ActiveRecord
+{
+    protected static $tableName = 'page';
+    protected $heading;
+    protected $content;
+    protected $created;
+
+    /**
+     * Get the value of heading
+     */ 
+    public function getHeading()
+    {
+        return $this->heading;
+    }
+
+    /**
+     * Set the value of heading
+     *
+     * @return  self
+     */ 
+    public function setHeading($heading)
+    {
+        $this->heading = $heading;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of content
+     */ 
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the value of content
+     *
+     * @return  self
+     */ 
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created
+     */ 
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set the value of created
+     *
+     * @return  self
+     */ 
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function save(): bool
+    {
+        if (!$this->id) {
+            $query = self::$pdo->prepare('INSERT INTO ' . self::$tableName . '(heading, content) VALUES(:heading, :content)');
+            $result = $query->execute([
+                'heading' => $this->heading,
+                'content' => $this->content
+            ]);
+    
+            if ($result) {
+                $insertedId = self::$pdo->lastInsertId();
+                $this->id = $insertedId;
+            }
+        } else {
+            $query = self::$pdo->prepare('UPDATE ' . self::$tableName . ' SET heading=:heading, content=:content WHERE id=:id');
+            $result = $query->execute([
+                'heading' => $this->heading,
+                'content' => $this->content,
+                'id' => $this->id
+            ]);
+        }
+        
+        return $result;
+    }
+}
--- index.php
+++ index.php
@@ -3,13 +3,21 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require_once 'Navigation.php';
+require_once 'Page.php';
 
-$allNavigation = Navigation::findAll();
+
+// $allNavigation = Navigation::findAll();
 $navigation1 = Navigation::findOne(1);
 
 var_dump($navigation1);
-var_dump($allNavigation);
-$navigation2 = new Navigation();
-$navigation2->save();
 
+$page1 = Page::findOne(1);
+var_dump($page1);
+
+// var_dump($allNavigation);
+$navigation2 = new Navigation();
+$navigation2->setTitle('My title');
+$navigation2->setPage_id(1);
+var_dump($navigation2);
+$navigation2->save();
 var_dump($navigation2);
